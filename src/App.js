de80448479d0a1f7d367c5083888447f0c0dfc57
Kevin <challengerenlol@gmail.com>
2022-08-20 14:52:39 -0300

    Home and Nav created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Nav from "./components/Nav.jsx";
+import Home from "./components/Home.jsx";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="">
+      <Nav />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Nav() {
+  return <div>hola nav</div>;
+}
+
+export default Nav;
